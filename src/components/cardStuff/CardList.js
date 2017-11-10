771c51b618b47e474389a25e198226782b24d0cf
ian-ramos <ramos.ianc@gmail.com>
2017-11-09 20:47:03 -0500

    added some notes regarding grid iteration

--- src/components/cardStuff/CardList.js
+++ src/components/cardStuff/CardList.js
@@ -11,9 +11,9 @@ const CardList = (props) => {
   for(let i = 0; i < cardsArr.length / numOfCols/* will always return 3 */; i++){  //usually 12 / 4 => 3 else 15 / 5 => 3
     let holder = [] //will hold all the rendered Card components
     for (let j = 0; j < numOfCols; j++){
-      holder.push(cardsArr[(i*numOfCols)+j]) //[0 * 4 + 0], [0 * 4 + 1]...[2 * 4 + 3] => for each row i, it will render out the correct number of cards
+      holder.push(cardsArr[(i*numOfCols)+j]) //grabbing each Card component. By multiplying it by the number of cols, we can grab the next section of 4 elements on the successive iteration.  Adding j allows us to grab the next element in that section
     }
-    formattedCardsArr.push(<Grid.Row columns={numOfCols} key={i/10}>{holder}</Grid.Row>)
+    formattedCardsArr.push(<Grid.Row columns={numOfCols} key={i/10}>{holder}</Grid.Row>) //once inner loop breaks, it'll push this to the formattedCardsArr)
   }
 
   return (
